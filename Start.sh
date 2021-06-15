4761f8b50a13190d6958921b87a010e6a544db90
UltraSive <31855485+UltraSive@users.noreply.github.com>
2021-06-15 16:55:47 -0500

    the hostname of the machine should now be rig-id

--- Start.sh
+++ Start.sh
@@ -1,2 +1,3 @@
-./xmrig -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=Test -p x --cpu-no-yield --cpu-max-threads-hint=50
+NAME="$(hostname)"
+./xmrig -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x --cpu-no-yield --cpu-max-threads-hint=50
 pause
