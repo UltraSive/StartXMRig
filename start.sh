776a5df12493bf8da3d5a8930f4c15aed4765c67
UltraSive <31855485+UltraSive@users.noreply.github.com>
2021-07-25 08:05:42 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 NAME="$(hostname)"
-./StartXMRig/xmrig-Linux/xmrig -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x --cpu-no-yield --cpu-max-threads-hint=50
+./StartXMRig/xmrig-Linux/xmrig -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x
