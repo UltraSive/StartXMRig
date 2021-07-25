64ecff59a23da0f8d93a9a2c5f52cc53e0d473db
UltraSive <31855485+UltraSive@users.noreply.github.com>
2021-07-24 21:12:57 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 sleep $[ ( $RANDOM % 60 ) + 1 ]s
 killall xmrig
-rm -rf ./StartXMRig/
+rm -rf ../StartXMRig/
 git clone https://github.com/UltraSive/StartXMRig.git
 chmod u+x ./StartXMRig/start.sh
 chmod u+x ./StartXMRig/update.sh
