411f4b8981518f5a8232783c950e17b74363b2aa
UltraSive <31855485+UltraSive@users.noreply.github.com>
2021-08-03 20:53:14 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-.\xmrig-Win10\xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=%ComputerName% -p x
+.\xmrig-Win10\xmrig.exe -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=%ComputerName% -p x
 pause
 
